b0c00461c0cae9a238dba9f540715b014b377a4d
manon <manon.orsini4@gmail.com>
2020-12-08 13:40:02 +0100

    mise en place bootsraps et base.html

---
+++ src/Entity/AisShipType.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AisShipTypeRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=AisShipTypeRepository::class)
+ */
+class AisShipType
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     @Assert\Length(min=1,
+     *          max=9,
+     *          minMessage = "Le type d'un navire est compris entre 1 et 9",
+     */
+    private $aisShipType;
+
+    /**
+     * @ORM\Column(type="string", length=60)
+     */
+    private $libelle;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getAisShipType(): ?int
+    {
+        return $this->aisShipType;
+    }
+
+    public function setAisShipType(int $aisShipType): self
+    {
+        $this->aisShipType = $aisShipType;
+
+        return $this;
+    }
+
+    public function getLibelle(): ?string
+    {
+        return $this->libelle;
+    }
+
+    public function setLibelle(string $libelle): self
+    {
+        $this->libelle = $libelle;
+
+        return $this;
+    }
+}
