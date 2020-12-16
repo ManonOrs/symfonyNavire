d8dc4e7a457ba1f9090476318d0b93e555eefb3c
manon <manon.orsini4@gmail.com>
2020-12-16 13:49:35 +0100

    searchbar

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -4,7 +4,12 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\NavireRepository;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
 
 class SearchController extends AbstractController
 {
@@ -22,7 +27,22 @@ class SearchController extends AbstractController
         ;
         
         return $this->render('search/search.html.twig', [
-            'controller_name' => 'SearchController',
+                    'formSearch' => $form->createView()
         ]);
+    }
+    
+    /**
+     * @Route("/search/handleshearch", name="search_handlesearch")
+     */
+    public function handleSearch(Request $request, NavireRepository $repo): Response {
+        $valeur = $request->request->get('form')['chercher'];
+        if (isset($request->request->get('form')['envoiimo'])) {
+            
+            $critere = "imo recherché : " . $valeure;
+        }else{
+            
+            $critere = "mmi recherché : " . $valeur;
+        }
+            return new Response("<h1> $critere </h1>");
     }
 }
